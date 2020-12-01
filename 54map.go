f44e26363821d9697b56af37a72e1d0112e7f549
manlan <2733284198@qq.com>
2020-12-01 17:48:59 +0800

    map如何赋值       初始化，没有赋值，是nil类型的，要判断的，
          map取值，

--- 54map.go
+++ 54map.go
@@ -23,6 +23,26 @@ func main() {
 	//map1[3] = {100:"hello"};
 	//map1[2] = "hello"
 
+	fmt.Println("\n ==>")
 	// todo:map如何赋值，
+	map2[1] = "map1"
+	map2[2] = "map2"
+	map2[100] = "map100"
+
+	fmt.Println(map2)
+	//fmt.Println(map2[100])
+
+	fmt.Println("\n ==>取值 ")
+	// 值不存在
+	value1, ok1 := map2[100]
+	//value2, ok2 := map2[200]
+
+	//fmt.Println(value1, ok1, map2[100])
+
+	if ok1 == true {
+		fmt.Println("v2值:", value1)
+	} else {
+		fmt.Println("操作的key不存在，获取到的是零值")
+	}
 
 }
