547528bd039a235f0555d9db9a89a2f4a13c73f7
manlan <2733284198@qq.com>
2020-12-02 03:06:13 +0800

    72 fun递归函数

--- 72fun.go
+++ 72fun.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("72 fun")
+	fmt.Println("\n\n ===>72 fun <====")
+
+	a := 2
+	fmt.Println(a)
+
+	defer fun1(a)
+	a++
+	fmt.Println(a)
+
+	//fun2()
+}
+
+func fun1(a int) int {
+	fmt.Println("72 fun1")
+
+	return a
+}
+
+func fun2() {
+	fmt.Println("72 fun2")
+
 }
